62f23efa60bcf02efaa13258ba4bb130fbbc9331
Angkasit Karoonchart <108455691+Marguro@users.noreply.github.com>
2026-04-09 00:24:44 +0700

    Add: implement FrameRateBootstrap to enforce a target frame rate of 60 FPS

---
+++ Assets/Scripts/Managers/FrameRateBootstrap.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Managers
+{
+    public static class FrameRateBootstrap
+    {
+        private const int TargetFrameRate = 60;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void ApplyFrameRateLimit()
+        {
+            QualitySettings.vSyncCount = 0;
+            Application.targetFrameRate = TargetFrameRate;
+
+#if UNITY_EDITOR
+            Debug.Log($"[FrameRateBootstrap] Applied frame cap: target={Application.targetFrameRate}, vSync={QualitySettings.vSyncCount}");
+#endif
+        }
+    }
+}
+
+
